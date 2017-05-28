606e724d035542f4fad8ff0fa964c1c4eb453bbd
Antonio <antonio.melpignano@gmail.com>
2017-05-28 18:52:55 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ACT2017
+
+open main.m
+set the  motors thrusts in f0 matrix and the torque coefficients jx jy jz.
+
+open mdlDerivatives
+set the 3 angles psi theta phi
--- main.m
+++ main.m
@@ -2,11 +2,11 @@ close all;
 clear all; 
 clc;
 
-jx = 12
+jx = 21
 jy = 23
 jz = 34
-f = [0 0 0]'
-f0 = [1 2 3 2]'; % 4 motor thrusts
+%f = [0 0 0]'
+f0 = [200 20 30 20]'; % 4 motor thrusts
 t = 0
 % p=[0 0 0]'
 MAXITER = 10
@@ -14,7 +14,8 @@ p = zeros(3,MAXITER);
 % Sampling the position of the quadrotor for integer time for 1 to MAXITER
 for t=1:1:MAXITER % t in seconds
 [sys,x0,str,ts] = mdlInitializeSizes(1);
-[sys pos f] = mdlDerivatives(t, x0, 13,jx,jy,jz,f,f0);
+[sys pos f0] = mdlDerivatives(t, x0, 13,jx,jy,jz,f0);
+%[sys pos f] = mdlDerivatives(t, x0, 13,jx,jy,jz,f,f0);
 % sys=mdlOutputs(1, x0, 13); 
 p(:,t) = pos;
 end
