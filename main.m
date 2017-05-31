ab8c058463ee476f706c6424322fab940c4d2e84
bigdata94new <simone.rizzo971033@live.it>
2017-05-31 11:37:00 +0200

    Add files via upload

--- main.m
+++ main.m
@@ -5,18 +5,24 @@ clc;
 jx = 21
 jy = 23
 jz = 34
-%f = [0 0 0]'
-f0 = [200 20 30 20]'; % 4 motor thrusts
+f0 = zeros(3,4);
+temp = [200 199 198 197];
+for i=1:4
+    f0(3,i) = temp(i); % 4 motor thrusts
+end
 t = 0
 % p=[0 0 0]'
-MAXITER = 10
-p = zeros(3,MAXITER);
+MAXITER = 100
+
+f=f0; % for first time
+temp = zeros(3,MAXITER);
 % Sampling the position of the quadrotor for integer time for 1 to MAXITER
 for t=1:1:MAXITER % t in seconds
+    
 [sys,x0,str,ts] = mdlInitializeSizes(1);
-[sys pos f0] = mdlDerivatives(t, x0, 13,jx,jy,jz,f0);
-%[sys pos f] = mdlDerivatives(t, x0, 13,jx,jy,jz,f,f0);
+% [sys pos f0] = mdlDerivatives_simone(t, x0, 13,jx,jy,jz,f0);
+[sys pos f] = mdlDerivatives(t, x0, 13,jx,jy,jz,f);
 % sys=mdlOutputs(1, x0, 13); 
-p(:,t) = pos;
+temp(:,t) = pos;
 end
-mdlOutputs(p);
+mdlOutputs(temp);
--- mdlDerivatives.m
+++ mdlDerivatives.m
@@ -3,27 +3,24 @@
 % mdlDerivatives
 % Return the derivatives for the continuous states.
 %=============================================================================
-%
-function [sys pos f0] = mdlDerivatives(t,x,u,jx,jy,jz,f0)
+
+function [sys pos f] = mdlDerivatives(t,x,u,jx,jy,jz,f)
 gg = [0 0 -9.81]';
- LL = 0.2; % lenght between two motors
+LL = 0.2; % lenght between two motors
 % bb = 1;
 
-
-
-
 %psi =deg2rad(90);              % vanno calcolate in base le 4 F...
-
-psi = arccos( LL/sqrt((f0(1,1)-f0(2,1))^2 +LL^2));
+psi = acos(LL/sqrt((f(3,1)-f(3,2))^2 +LL^2));
 %theta=deg2rad(60);
-theta= arccos( LL/sqrt((f0(1,1)-f0(3,1))^2 +LL^2));
+theta = acos(LL/sqrt((f(3,1)-f(3,3))^2 +LL^2));
 %phi=deg2rad(30);
-phi= arccos( LL/sqrt((f0(1,1)-f0(4,1))^2 +LL^2));
+phi= acos(LL/sqrt((f(3,1)-f(3,4))^2 +LL^2));
 g=9.81;
 
 % p=[0 0 0]'; % initial position vector
-v=[sum(f0)*cos(psi) sum(f0)*cos(theta) sum(f0)*cos(phi)]'; % linear velocity        % vanno calcolate in base le 4 F...
-w=[diff(psi,t) diff(theta,t) diff(phi,t)]'; % angular velocity  d(angle)/dt
+v=[sum(f(3,:))*cos(psi) sum(f(3,:))*cos(theta) sum(f(3,:))*cos(phi)]'; % linear velocity        % vanno calcolate in base le 4 F...
+% w=[diff(psi,t), diff(theta,t), diff(phi,t)]'; % angular velocity  d(angle)/dt
+w = [psi/t theta/t phi/t]';
 gamma =[0 0 0]'; % torques vector
 % f = 100  % it is the sum of four motor trusts
 m = 20; % quadrotor mass
@@ -35,7 +32,7 @@ e3= [0 0 1]';
 
 J=diag([jx jy jz]);
 
-Omega = cross(w, J*w);
+% Omega = cross(w, J*w);
 
 % standard quadrotor model
 %p_dot= R*v;
@@ -65,26 +62,27 @@ ev= [1/v(1,1) 1/v(2,1) 1/v(3,1)]'; % versore rispetto la direzione di velocit?
 ed= [0 0 -1]'; %  versore in direzione z-
 
 
-D = (1/(2*p))*(v).^2*Cd*A; % drag coefficient
+D = diag((1/(2*p))*(v).^2*Cd*A); % drag coefficient
 
 
-
-f = -D*ev+m*gg+R*sum(f0)*e3; % f with aerodynamics drag
-
+%% DYNAMICS
 phi_dot= w(1,1) + w(2,1)*sin(phi)*tan(theta)+ w(3,1)*cos(phi)*tan(theta);
 theta_dot= w(2,1)*cos(phi)-w(3,1)*sin(phi);
 psi_dot = w(2,1)*sin(phi)*(1/cos(theta))+ w(3,1)*cos(phi)*(1/cos(theta));
 
 v1_dot = w(3,1)*v(2,1)- w(2,1)*v(3,1)-g*sin(theta);
 v2_dot = -w(3,1)*v(1,1) + w(1,1)*v(3,1)+g*sin(phi)*cos(theta);
-v3_dot = w(2,1)*v(1,1)- w(1,1)*v(2,1)+g*cos(theta)*cos(phi)-f/m;
+v3_dot = w(2,1)*v(1,1)- w(1,1)*v(2,1)+g*cos(theta)*cos(phi)-sum(f(3,:))/m;
 
 
 p_dot= w(2,1)*w(3,1)*((jy-jz)/jx) + gamma(1,1)/jx;
 q_dot= w(1,1)*w(3,1)*((jz-jx)/jy) + gamma(2,1)/jy;
 r_dot= w(1,1)*w(2,1)*((jx-jy)/jz) + gamma(3,1)/jz;
 
-
+% Component by component
+for i=1:4
+    f(:,i) = f(:,i)-D*ev+m*gg+R*f(:,i); % f with aerodynamics drag
+end
 
 
 sys = [phi_dot theta_dot psi_dot;v1_dot v2_dot v3_dot;p_dot q_dot r_dot];
